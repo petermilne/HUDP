f27041f46b6d6ec670ef621c63d25d18815aaf6b
peter milne <peter.milne@naboo>
2022-11-22 18:19:18 +0000

    using ITIMER_REAL: this does a nice 20kHz ./udp_client 10.12.198.128 53676 128 shot_data 50
    also, rewinds itself, no need to put in a loop
    	modified:   udp_client.c

--- udp_client.c
+++ udp_client.c
@@ -8,8 +8,15 @@
 #include <assert.h>
 #include <unistd.h>
 
+#include <sys/time.h>
+#include <signal.h>
+
 #define MAXDGRAM 10240
 
+void missed_alarm(int signum)
+{
+
+}
 int main(int argc, char**argv)
 {
    	int sockfd;
@@ -20,7 +27,15 @@ int main(int argc, char**argv)
 	int nb;
 	int maxdgram = MAXDGRAM;
 	FILE* fp;
-	int usecs = 1000;
+	struct itimerval timer;
+	sigset_t alarm_sig;
+	int signum;
+
+	sigemptyset(&alarm_sig);
+	sigaddset(&alarm_sig, SIGALRM);
+	signal(SIGALRM, missed_alarm);
+
+	timer.it_value.tv_sec = timer.it_interval.tv_sec = 0;
 
 	if (getenv("MAXDGRAM")){
 		maxdgram = atoi(getenv("MAXDGRAM"));		
@@ -41,11 +56,18 @@ int main(int argc, char**argv)
 	maxdgram = atoi(argv[3]);
 	fp = fopen(argv[4], "r");
 	assert(fp);
-	usecs = atoi(argv[5]);
+	timer.it_value.tv_usec = timer.it_interval.tv_usec = atoi(argv[5]);
 
-	while((nb = fread(sendbuf, 1, maxdgram, fp)) > 0){
-		sendto(sockfd, sendbuf, nb, 0, ADDRP, ADDRSZ);
-		usecs==0 || usleep(usecs);
+	if (setitimer(ITIMER_REAL, &timer, 0) < 0) {
+	    perror("setitimer");
+	    exit(1); 
+	}
+	while(1){
+		while((nb = fread(sendbuf, 1, maxdgram, fp)) > 0){
+			sigwait(&alarm_sig, &signum);
+			sendto(sockfd, sendbuf, nb, 0, ADDRP, ADDRSZ);
+		}
+		rewind(fp);
 	}
 	return 0;
 }
